2a2ffc7c934bed5c76e868a1d629fd4f29ff9381
Michael Sam <sam.michael.h@gmail.com>
2023-11-07 14:09:51 +0900

    Made task manager. Need to refactor and add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #SHOW ALL
+  get "tasks", to: "tasks#index"
+
+  #CREATE
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  #READ
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #UPDATE
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  #DELETE
+  delete "tasks/:id", to: "tasks#destory"
 end
